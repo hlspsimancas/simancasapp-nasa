e4173ceb9c0e9d526928390a49708ffebf4e8742
Hector L Simancas P <hlspsimancas@gmail.com>
2020-03-11 20:45:51 -0430

    Updates

---
+++ precache-manifest.2038097bc04fdfb183a7904317c70713.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eec09713374c663f930be7565cf041fe",
+    "url": "/simancasapp-nasa/index.html"
+  },
+  {
+    "revision": "c9d38484939ff46b48e0",
+    "url": "/simancasapp-nasa/static/css/2.e831bed6.chunk.css"
+  },
+  {
+    "revision": "cfdc3e6ccdb85fc87c48",
+    "url": "/simancasapp-nasa/static/css/main.d328b621.chunk.css"
+  },
+  {
+    "revision": "c9d38484939ff46b48e0",
+    "url": "/simancasapp-nasa/static/js/2.64d04aea.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/simancasapp-nasa/static/js/2.64d04aea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfdc3e6ccdb85fc87c48",
+    "url": "/simancasapp-nasa/static/js/main.7465d9ca.chunk.js"
+  },
+  {
+    "revision": "0d7abc6f48d2c74ac021",
+    "url": "/simancasapp-nasa/static/js/runtime-main.ab793d48.js"
+  },
+  {
+    "revision": "654d4fd7957b7388d110ddfc2e9c71cc",
+    "url": "/simancasapp-nasa/static/media/astronaut.654d4fd7.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/simancasapp-nasa/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/simancasapp-nasa/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/simancasapp-nasa/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/simancasapp-nasa/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/simancasapp-nasa/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "75b166bd406a4d4eb4f9b996e35136a8",
+    "url": "/simancasapp-nasa/static/media/iss.75b166bd.jpeg"
+  },
+  {
+    "revision": "9bd0276973230d5ecf30d8ee69d06f90",
+    "url": "/simancasapp-nasa/static/media/moon-bg.9bd02769.jpg"
+  },
+  {
+    "revision": "179e7e58ea904260550df26805883d58",
+    "url": "/simancasapp-nasa/static/media/space-bg.179e7e58.jpg"
+  }
+]);
